a562a0382dbe479a6299a43c6f6263301c8bd9f8
Rafi Ariel <65024803+RafiAriel@users.noreply.github.com>
2022-01-10 13:16:15 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGODB_URI: 'mongodb://raful:raful123@cluster0-shard-00-00.pxlkg.mongodb.net:27017,cluster0-shard-00-01.pxlkg.mongodb.net:27017,cluster0-shard-00-02.pxlkg.mongodb.net:27017/cmscart?retryWrites=true&w=majority&replicaSet=atlas-j324hu-shard-0&ssl=true&authSource=admin'
+    MONGODB_URI: # My link Connection MongoDB
 }
